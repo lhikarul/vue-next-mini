f1f1e18aea4af5b63166d4de596e811bb565153b
lhikarul <711hikaru@gmail.com>
2023-02-16 16:47:29 +0800

    feat: 5-8 構建 effect 函數, 生成 ReactiveEffect 實例

--- packages/reactivity/src/effect.ts
+++ packages/reactivity/src/effect.ts
@@ -1,3 +1,19 @@
+export function effect<T = any>(fn: () => T) {
+  const _effect = new ReactiveEffect(fn)
+  _effect.run()
+}
+
+export let activeEffect: ReactiveEffect | undefined
+
+export class ReactiveEffect<T = any> {
+  constructor(public fn: () => T) {}
+
+  run() {
+    activeEffect = this
+    return this.fn()
+  }
+}
+
 export function track(target: object, key: unknown) {
   console.log('track')
 }

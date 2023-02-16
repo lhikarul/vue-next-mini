564ed6190aa29f6c28679514c6bb0762e2219463
lhikarul <711hikaru@gmail.com>
2023-02-16 16:58:56 +0800

    feat: 5-10 構建 track 依賴收集函數

--- packages/reactivity/src/effect.ts
+++ packages/reactivity/src/effect.ts
@@ -1,3 +1,7 @@
+type KeyToDepMap = Map<any, ReactiveEffect>
+
+const targetMap = new WeakMap<any, KeyToDepMap>()
+
 export function effect<T = any>(fn: () => T) {
   const _effect = new ReactiveEffect(fn)
   _effect.run()
@@ -15,7 +19,15 @@ export class ReactiveEffect<T = any> {
 }
 
 export function track(target: object, key: unknown) {
-  console.log('track')
+  if (!activeEffect) return
+  let depsMap = targetMap.get(target)
+  if (!depsMap) {
+    targetMap.set(target, (depsMap = new Map()))
+  }
+
+  depsMap.set(key, activeEffect)
+
+  console.log('targetMap ', targetMap)
 }
 
 export function trigger(target: object, key: unknown, newValue: unknown) {
